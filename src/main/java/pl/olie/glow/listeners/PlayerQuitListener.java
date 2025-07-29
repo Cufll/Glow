e205a513904a8322dae523af8c5eb6c6c17695b3
karolekk12 <karol2006556@gmail.com>
2025-07-29 16:57:25 +0200

    move to PlayerQuitListener

--- src/main/java/pl/olie/glow/listeners/PlayerQuitListener.java
+++ src/main/java/pl/olie/glow/listeners/PlayerQuitListener.java
@@ -1,7 +1,24 @@
 package pl.olie.glow.listeners;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import pl.olie.glow.Glow;
+import pl.olie.glow.config.Config;
 
 public class PlayerQuitListener implements Listener {
-
+    private final Config config;
+    public PlayerQuitListener(Glow plugin) {
+        this.config = plugin.getConfigValues();
+    }
+    @EventHandler
+    public void onPlayerLeft(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        if(config.getOffOnLeft()){
+            if(player.isGlowing()) {
+                player.setGlowing(false);
+            }
+        }
+    }
 }
